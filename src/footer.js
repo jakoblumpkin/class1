9b30415fff195d7dcc7ca131341cca017bf0208c
jakoblumpkin <lumpkinjakobr@gmail.com>
2021-03-16 19:36:14 -0500

    class2cc

--- src/footer.js
+++ src/footer.js
@@ -4,7 +4,7 @@ class Footer extends React.Component {
   render(){
     return (
       <div>
-        <p>&copy; Jakob Lumpkin </p>      
+        <p>&copy; Jakob Lumpkin </p>
       </div>
     );
   }
